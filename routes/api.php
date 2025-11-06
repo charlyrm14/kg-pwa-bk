da81cc7390bcfbbbb8aca239e4f0fdb219fab06c
charlyrm14 <charlyrm14@gmail.com>
2025-11-06 11:52:20 -0600

    refactor(update-user-profile-info): se refactoriza actualizacion de informacion de perfil se usuario autenticado

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::prefix('v1/')->group(function () {
     Route::prefix('users/')->controller(UserController::class)->group(function() {
         Route::post('', 'store');
         Route::put('{uuid}', 'update');
-        Route::put('{uuid}/profile', 'updateProfileInfo');
+        Route::put('profile/info', 'updateProfileInfo')->middleware(['passport.cookie', 'auth:api']);
         Route::put('{uuid}/hobbies', 'updateHobbies');
         Route::delete('{uuid}', 'delete');
     });
