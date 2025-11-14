0462e4ea84a18bba705ebb603f231db2e81afac1
charlyrm14 <charlyrm14@gmail.com>
2025-11-13 18:28:37 -0600

    feature(contents-list): se agrega listado de contenidos

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -4,21 +4,65 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\{
+    Content
+};
 use App\Http\Requests\Content\{
     StoreContentRequest
 };
 use App\Http\Resources\Content\{
+    IndexCollection,
     NewContentResource
 };
 use App\Services\Content\ContentManager;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class ContentController extends Controller
 {
     public function __construct(
         protected ContentManager $manager
     ){}
+
+    /**
+     * The index function retrieves a list of content based on certain conditions and returns it as a
+     * JSON response.
+     * 
+     * @param Request request The `index` function is a controller method that retrieves a list of
+     * content based on certain conditions. Here's an explanation of the code:
+     * 
+     * @return JsonResponse A JSON response is being returned with a data key containing the result of
+     * the IndexCollection transformation of the contents fetched from the database. If successful, the
+     * response will have a status code of 200. If an error occurs during the process, a JSON response
+     * with an error message and a status code of 500 will be returned.
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try {
+
+            $query = Content::query();
+
+            if(!($request->user() && $request->user()->role_id === 1)) {
+                $query->where('content_status_id', 5);
+            } 
+
+            $contents = $query->with(['type', 'status', 'user', 'event'])->paginate(15);
+
+            return response()->json([
+                'data' => new IndexCollection($contents)
+            ], 200);
+
+        } catch(\Throwable $e) {
+
+            Log::error('Error to get content list: ' . $e->getMessage());
+            
+            return response()->json([
+                'message' => 'Error to get content list',
+            ], 500);
+
+        }
+    }
 
     /**
      * Store a newly event created resource in storage.
---
+++ app/Http/Resources/Content/IndexResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Content;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class IndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->name,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'author' => $this->whenLoaded('user')->name ?? 'unknown',
+            'type' => $this->whenLoaded('type')->name ?? 'unknown',
+            'status' => $this->whenLoaded('status')->name ?? 'unknown',
+            'event' => $this->whenLoaded('event', function() {
+                return [
+                    'location' => $this->event->location,
+                    'start_date' => $this->event->start_date,
+                    'end_date' => $this->end_date
+                ];
+            })
+        ];
+    }
+}
