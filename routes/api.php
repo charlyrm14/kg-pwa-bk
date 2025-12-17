42dcda5f28f530b9ce4505ed78ee99fbab4acb43
charlyrm14 <charlyrm14@gmail.com>
2025-12-17 11:45:31 -0600

    refactor(swim-categories): se modifica respuesta de progreso de estudiante

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,8 @@ Route::prefix('v1/')->group(function () {
     });
 
     Route::prefix('student-progress/')->controller(StudentProgressController::class)->group(function() {
+        Route::post('', 'assignProgress');
+        Route::get('{uuid}', 'dataProgress');
     });
 
     Route::get('hobbies', HobbyController::class);
