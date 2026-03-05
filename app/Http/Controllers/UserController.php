65996bfd54089fc4216c1ac1ee6407591b95e269
charlyrm14 <charlyrm14@gmail.com>
2026-03-05 16:21:04 -0600

    feat(user-info-progress): se agrega informacion de progreso de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
     /**
      * The function retrieves a paginated list of users with their roles and returns it as a JSON
      * response, handling errors appropriately.
-     * 
+     *
      * @return JsonResponse The `index` function returns a JSON response. If the list of users is
      * successfully retrieved, it returns a JSON response with a status code of 200 and the data in the
      * response body is formatted using the `IndexCollection`. If the list of users is empty, it
@@ -64,7 +64,7 @@ class UserController extends Controller
     
     /**
      * Store a newly created resource in storage.
-     * 
+     *
      * @param StoreUserRequest request The validated request object containing user data.
      * @return JsonResponse A JSON response indicating success or failure of user creation.
      */
@@ -92,10 +92,10 @@ class UserController extends Controller
     /**
      * The function `show` retrieves a user's data and related information, handling any errors that
      * may occur.
-     * 
+     *
      * @param User user The `show` function is a controller method that returns a JSON response with
      * data about a specific user. Here's a breakdown of the parameters used in the function:
-     * 
+     *
      * @return JsonResponse A JSON response is being returned. If the operation is successful, it will
      * return a JSON response with the data of the user, including their role, schedules for each day,
      * and attendances for the current month. If an error occurs during the process, it will return a
@@ -104,9 +104,19 @@ class UserController extends Controller
     public function show(User $user): JsonResponse
     {
         try {
+
+            $data = $user->load(
+                    'role',
+                    'schedules.day',
+                    'attendancesCurrentMonth'
+                );
+
+            $studentProgram = $user->studentPrograms()->with(['categories.swimCategory'])->first();
             
+            $data->studentPrograms = $studentProgram;
+                        
             return response()->json([
-                'data' => new ShowUserResource($user->load('role', 'schedules.day', 'attendancesCurrentMonth'))
+                'data' => new ShowUserResource($data)
             ], 200);
 
         } catch(\Throwable $e) {
@@ -121,10 +131,10 @@ class UserController extends Controller
 
     /**
      * Update the basic information from a specific user in storage
-     * 
+     *
      * @param UpdateUserInfoRequest request The validated request object containing information data.
      * @param string uuid The UUID of the user whose information is to be updated.
-     * 
+     *
      * @return JsonResponse A JSON response indicating success or failure of user information updated.
      */
     public function update(UpdateUserInfoRequest $request, string $uuid): JsonResponse
@@ -156,9 +166,9 @@ class UserController extends Controller
 
     /**
      * Delete the specified user from storage
-     * 
+     *
      * @param string uuid The UUID of the user whose hobbies are updated
-     * 
+     *
      * @return JsonResponse a JSON response indicating success or failure of a user deleted
      */
     public function destroy(string $uuid): JsonResponse
